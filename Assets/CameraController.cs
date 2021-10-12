41ee49d4e98bc73e723ec82dc4df0d34928b57b7
Don Nguyen <59811507+duong852@users.noreply.github.com>
2021-10-12 01:57:40 -0400

    added basic tile map, clamp the movement of camera to the border of the map

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,14 +7,30 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     public Transform Player;
     private Vector3 offset;
+    public float MIN_X;
+    public float MAX_X;
+    public float MIN_Y;
+    public float MAX_Y;
     void Start()
     {
         offset = transform.position - Player.transform.position;
     }
 
+    //private void Update()
+    //{
+    //    transform.position = new Vector3(
+    //    Mathf.Clamp(Player.transform.position.x, MIN_X, MAX_X),
+    //    Mathf.Clamp(Player.transform.position.y, MIN_Y, MAX_Y),
+    //    transform.position.z);
+    //}
+
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = Player.transform.position + offset;
+        transform.position = new Vector3(
+        Mathf.Clamp(transform.position.x, MIN_X, MAX_X),
+        Mathf.Clamp(transform.position.y, MIN_Y, MAX_Y),
+        transform.position.z);
     }
 }
