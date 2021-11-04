c7e09059bc0fe6c924f25b46dd47ad6ab2fe8f82
NitsuaKehn <arkeene@mtu.edu>
2021-11-04 11:19:02 -0400

    Game Over Menu

---
+++ Assets/StoreMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreMenu : MonoBehaviour
+{
+    //This Script will be used for the Store menu
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UserInterface.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    //This Script will be used for the main player UI. //Ex. spells
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
